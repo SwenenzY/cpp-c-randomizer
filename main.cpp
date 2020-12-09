b0fff5b643a77ea837d0a30b4ee3902974a5b38b
Yiğit Kaan Bayat <ykaanbayat@gmail.com>
2020-12-09 20:06:37 +0300

    First Upload - Guid

---
+++ main.cpp
@@ -0,0 +1,46 @@
+// Common Includes
+#include <windows.h>
+#include <iomanip>
+
+// STD
+#include <string>
+
+// Stream
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ostream>
+
+std::string CreateGuid() {
+    GUID guid;
+    CoCreateGuid(&guid);
+
+    std::ostringstream os;
+    os << std::hex << std::setw(8) << std::setfill('0') << guid.Data1;
+    os << '-';
+    os << std::hex << std::setw(4) << std::setfill('0') << guid.Data2;
+    os << '-';
+    os << std::hex << std::setw(4) << std::setfill('0') << guid.Data3;
+    os << '-';
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[0]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[1]);
+    os << '-';
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[2]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[3]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[4]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[5]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[6]);
+    os << std::hex << std::setw(2) << std::setfill('0') << static_cast<short>(guid.Data4[7]);
+
+    std::string s(os.str());
+
+    return s;
+}
+
+int main()
+{
+    SetConsoleTitleA("Randomizer []");
+    
+    std::cout << CreateGuid() << std::endl;
+
+}
