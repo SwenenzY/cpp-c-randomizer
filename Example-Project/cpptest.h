9ff124c760d63a303d77e0669ad7df1988e9e58e
Yiğit Kaan Bayat <ykaanbayat@gmail.com>
2020-12-19 04:28:57 +0300

    Upload

--- Example-Project/cpptest.h
+++ Example-Project/cpptest.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "stdafx.h"
 
-void O_O TestVoid1 o_o (int TestInt1, std::string TestString1);
+void O_O TestVoid1 o_o (int O_O TestInt1 o_o, std::string O_O TestString1 o_o);
 
-namespace TestNamespace {
-	bool O_O TestNamespaceVoid1 o_o (bool TestBool1);
+namespace O_O TestNamespace o_o {
+	bool O_O TestNamespaceVoid1 o_o (bool O_O TestBool1 o_o);
 }
 
 class O_O TestClass o_o {
 public:
-	static float O_O MyClassVoid1 o_o (float MyFlaot1 = 3.f);
-	static void  O_O MyClassVoid2 o_o (float MyFloat2);
+	static float O_O MyClassVoid1 o_o (float O_O MyFlaot1 o_o = 3.f);
+	static void  O_O MyClassVoid2 o_o (float O_O MyFloat2 o_o);
 	static void  O_O MyClassVoid3 o_o ();
 private:
 	static void O_O MyclassVoid4 o_o ();
